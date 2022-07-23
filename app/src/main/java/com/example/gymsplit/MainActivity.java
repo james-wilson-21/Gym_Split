fa6a92d21f3151daeb3dfe8202a3bbc8d07799a3
jamoTown <treppiejames@hotmail.com>
2022-07-24 10:27:24 +1200

    Make changes to the manifest file and preview code on main activity

--- app/src/main/java/com/example/gymsplit/MainActivity.java
+++ app/src/main/java/com/example/gymsplit/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.gymsplit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        Have a go at this later vv
+//        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
