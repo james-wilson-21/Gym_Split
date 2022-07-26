c9448b1a6e54870b022a7ec6671cc62e6d44b183
jamoTown <treppiejames@hotmail.com>
2022-07-27 09:18:42 +1200

    Add handler and method to delay splash screen

--- app/src/main/java/com/example/gymsplit/MainActivity.java
+++ app/src/main/java/com/example/gymsplit/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewTreeObserver;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Boolean variable
+    boolean isAndroidReady = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -19,15 +21,28 @@ public class MainActivity extends AppCompatActivity {
         View content = findViewById(android.R.id.content);
 
         // Add pre-draw listener to view tree observer
-//        content.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-//            @Override
-//            public boolean onPreDraw() {
-//                return false;
-//            }
-//        });
+        content.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                if (isAndroidReady) {
+                    content.getViewTreeObserver().removeOnPreDrawListener(this);
+                }
+
+                // Method for isAndroidReady boolean
+                dismissSplashScreen();
+                return false;
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    private void dismissSplashScreen() {
+        // establish handler
+        new Handler().postDelayed(() -> {
+            // change boolean
+            isAndroidReady = true;
+        }, 2000);
+    }
 }
