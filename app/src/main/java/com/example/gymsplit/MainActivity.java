fdfa8cef90135a8d78ae3c93450a38e11bf9c2c0
jamoTown <treppiejames@hotmail.com>
2022-07-26 19:03:07 +1200

    Add splash screen handler to Main Activity

--- app/src/main/java/com/example/gymsplit/MainActivity.java
+++ app/src/main/java/com/example/gymsplit/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.gymsplit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
-import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        Have a go at this later vv
-//        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        // Handle the splash screen transition
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
