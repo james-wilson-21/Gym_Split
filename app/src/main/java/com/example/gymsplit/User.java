368fc7d3a43bbcc5ab4ebf0c1a3eb6b1fd89c52f
jamoTown <treppiejames@hotmail.com>
2022-07-29 18:46:56 +1200

    Setup database User Class entity

--- app/src/main/java/com/example/gymsplit/User.java
+++ app/src/main/java/com/example/gymsplit/User.java
@@ -1,2 +1,32 @@
-package com.example.gymsplit;public class User {
+package com.example.gymsplit;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "User") // "User" refers to the table name, not the Class below
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "ID")
+    private int id;
+
+    @ColumnInfo(name="UserName")
+    private String userName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
