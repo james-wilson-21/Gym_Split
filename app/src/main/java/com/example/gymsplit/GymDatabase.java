90e6ad2ca98731d792e7e29e0c86388565a73d8f
jamoTown <treppiejames@hotmail.com>
2022-08-02 17:54:34 +1200

    Add Workout class and WorkoutDao Interface

--- app/src/main/java/com/example/gymsplit/GymDatabase.java
+++ app/src/main/java/com/example/gymsplit/GymDatabase.java
@@ -3,7 +3,8 @@ package com.example.gymsplit;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Workout.class}, version = 2)
 public abstract class GymDatabase extends RoomDatabase {
     public abstract UserDao userDao();
+    public abstract WorkoutDao workoutDao();
 }
---
+++ app/src/main/java/com/example/gymsplit/Workout.java
@@ -0,0 +1,45 @@
+package com.example.gymsplit;
+
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Workout") // "Workout" refers to the table name, not the Class below
+
+ public class Workout {
+
+ @PrimaryKey(autoGenerate = true)
+ @ColumnInfo(name = "ID")
+ private int id;
+
+ @ColumnInfo(name="workoutName")
+ private String workoutName;
+
+ @ColumnInfo(name = "splitDays")
+ private int splitDays;
+
+ public int getId() {
+  return id;
+ }
+
+ public void setId(int id) {
+  this.id = id;
+ }
+
+ public String getWorkoutName() {
+  return workoutName;
+ }
+
+ public void setWorkoutName(String workoutName) {
+  this.workoutName = workoutName;
+ }
+
+ public int getSplitDays() {
+  return splitDays;
+ }
+
+ public void setSplitDays(int splitDays) {
+  this.splitDays = splitDays;
+ }
+}
