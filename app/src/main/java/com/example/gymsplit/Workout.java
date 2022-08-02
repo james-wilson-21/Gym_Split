ab4e7c50a8ea3f215b0ddb2c09d8f50363c44633
jamoTown <treppiejames@hotmail.com>
2022-08-02 18:17:48 +1200

    Commit and push into main

--- app/src/main/java/com/example/gymsplit/Workout.java
+++ app/src/main/java/com/example/gymsplit/Workout.java
@@ -5,7 +5,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "Workout") // "Workout" refers to the table name, not the Class below
+@Entity(tableName = "Workout") // "Workout" refers to the table name, not the Class below;
 
  public class Workout {
 
