910648d9c05007bc0ce371ab713a4a193b2cab80
jamoTown <treppiejames@hotmail.com>
2022-08-24 13:08:35 +1200

    Add variables to main activity and removed User Class & Dao.

--- app/src/main/java/com/example/gymsplit/GymDatabase.java
+++ app/src/main/java/com/example/gymsplit/GymDatabase.java
@@ -3,8 +3,7 @@ package com.example.gymsplit;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class, Workout.class}, version = 2)
+@Database(entities = {Workout.class}, version = 1)
 public abstract class GymDatabase extends RoomDatabase {
-    public abstract UserDao userDao();
     public abstract WorkoutDao workoutDao();
 }
