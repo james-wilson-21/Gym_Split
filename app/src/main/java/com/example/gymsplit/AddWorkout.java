2d5a175dc3cc3268a24afc8a4075f44a369220d5
James <treppiejames@hotmail.com>
2023-08-20 10:37:59 +1200

    Add screen for new workout and split days input.

--- app/src/main/java/com/example/gymsplit/AddWorkout.java
+++ app/src/main/java/com/example/gymsplit/AddWorkout.java
@@ -3,12 +3,28 @@ package com.example.gymsplit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AddWorkout extends AppCompatActivity {
+
+    // Initialise variables
+    public static GymDatabase gymDatabase;
+    TextView workOutName, splitDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_workout);
+
+        workOutName = findViewById(R.id.addWorkoutName);
+        splitDays = findViewById(R.id.addSplitDays);
+
+        String addWorkoutName = getResources().getString(R.string.add_workout_name);
+        String addSplitDays = getResources().getString(R.string.add_split_days);
+
+        workOutName.setText(addWorkoutName);
+        splitDays.setText(addSplitDays);
+
+
     }
 }
