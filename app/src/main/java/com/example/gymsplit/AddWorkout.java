ce76c63f5604558c71516cd8906225ebc12ad3ee
James <treppiejames@hotmail.com>
2023-08-20 11:57:42 +1200

    Add new DayOne activity and class to receive extras

--- app/src/main/java/com/example/gymsplit/AddWorkout.java
+++ app/src/main/java/com/example/gymsplit/AddWorkout.java
@@ -6,14 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AddWorkout extends AppCompatActivity {
 
     // Initialise variables
-    public static GymDatabase gymDatabase;
     TextView workOutName, splitDays;
-
+    EditText workoutInput, splitInput;
     Button dayOneButton, cancelButton;
 
     @Override
@@ -26,6 +26,8 @@ public class AddWorkout extends AppCompatActivity {
         splitDays = findViewById(R.id.addSplitDays);
         dayOneButton = findViewById(R.id.dayOneButton);
         cancelButton = findViewById(R.id.cancelButton);
+        workoutInput = findViewById(R.id.addWorkoutInput);
+        splitInput = findViewById(R.id.addSplitDaysInput);
 
         String addWorkoutName = getResources().getString(R.string.add_workout_name);
         String addSplitDays = getResources().getString(R.string.add_split_days);
@@ -42,6 +44,17 @@ public class AddWorkout extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), MainMenu.class));
                 finish();
+            }
+        });
+
+        dayOneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(AddWorkout.this, DayOne.class);
+                intent.putExtra("keyname", workoutInput.getText().toString());
+                intent.putExtra("keySplit", splitInput.getText().toString());
+                startActivity(intent);
             }
         });
     }
